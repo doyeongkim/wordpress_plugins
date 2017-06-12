e244e4e4921da097953a89b174f1fa052df0a2f3
doyeongkim <rlaehdud19@gmail.com>
2017-06-12 18:51:27 +0800

    Update datetime.js
    Delete all that codes except the function() part

--- js/datetime.js
+++ js/datetime.js
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script>
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -17,12 +13,3 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
-</script>
-</head>
-
-<body onload="startTime()">
-
-<div id="txt"></div>
-
-</body>
-</html>
