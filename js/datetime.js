879d92b182a57a368ebfd7c95f858275bc4aaaf2
doyeongkim <rlaehdud19@gmail.com>
2017-06-12 18:45:30 +0800

    Create datetime.js

--- js/datetime.js
+++ js/datetime.js
@@ -1,3 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -13,3 +17,12 @@ function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+</script>
+</head>
+
+<body onload="startTime()">
+
+<div id="txt"></div>
+
+</body>
+</html>
