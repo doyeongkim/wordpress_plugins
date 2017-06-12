f2588eeec7fc5d8f02ba3b0c9458291276f6d6ba
doyeongkim <rlaehdud19@gmail.com>
2017-06-12 18:28:58 +0800

    first commit

---
+++ datetime.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: Date Time
+Description: Wordpress plugin for datetime
+Version: 040617
+Author: Kim
+*/
+
+function datetime_js_add() {
+   wp_register_script('datetime-script', plugins_url('/js/datetime.js', __FILE__));
+
+    wp_enqueue_script('datetime-script');
+}
+
+add_action("wp_enqueue_scripts", "datetime_js_add");
+
+?>
